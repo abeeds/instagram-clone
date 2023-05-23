672674d5e1f025f47898a49a895a1350270718c8
Shahria Abeed <shahriaabeed@gmail.com>
2023-05-23 19:28:42 -0400

    Navbar tweaks & new icons

--- src/navbar.tsx
+++ src/navbar.tsx
@@ -7,6 +7,7 @@ import {ReactComponent as MessageIcon} from './images/message.svg';
 import {ReactComponent as NotificationsIcon} from './images/notifications.svg';
 import {ReactComponent as CreateIcon} from './images/create.svg';
 import {ReactComponent as TempI} from './images/temp.svg';
+import {ReactComponent as MoreIcon} from './images/more.svg';
 
 // This function is responsible for creating the navbar
 // TODO: insert the SVGs before each word. Profile needs to show the profile picture before it
@@ -14,34 +15,55 @@ import {ReactComponent as TempI} from './images/temp.svg';
 function NavBar() {
     return(<nav className='navbar'>
         <ul className='navbar-nav'>
-            <li className='nav-logo'><a className='logo-text' href='https://www.youtube.com/watch?v=dQw4w9WgXcQ&pp=ygUJcmljayByb2xs'>INSTA CLONE</a></li>
-            <li className='nav-item'>
-                <div className='navicon'><HomeIcon/></div>
-                <span className='navtext'>Home</span>
+            <li className='nav-logo'><a className='logo-text' href='https://www.youtube.com/watch?v=dQw4w9WgXcQ&pp=ygUJcmljayByb2xs'>Instagraham</a></li>
+            <li className='nav-list'>
+                <div className='nav-item'>
+                    <div className='navicon'><HomeIcon/></div>
+                    <span className='navtext'>Home</span>
+                </div>
             </li>
-            <li className='nav-item'>
-                <div className='navicon'><SearchIcon/></div>
-                <span className='navtext'>Search</span>
+            <li className='nav-list'>
+                <div className='nav-item'>
+                    <div className='navicon'><SearchIcon/></div>
+                    <span className='navtext'>Search</span>
+                </div>
             </li>
-            <li className='nav-item'>
-                <div className='navicon'><ExploreIcon/></div>
-                <span className='navtext'>Explore</span>
+            <li className='nav-list'>
+                <div className='nav-item'>
+                    <div className='navicon'><ExploreIcon/></div>
+                    <span className='navtext'>Explore</span>
+                </div>
             </li>
-            <li className='nav-item'>
-                <div className='navicon'><MessageIcon/></div>
-                <span className='navtext'>Messages</span>
+            <li className='nav-list'>
+                <div className='nav-item'>
+                    <div className='navicon'><MessageIcon/></div>
+                    <span className='navtext'>Messages</span>
+                </div>
             </li>
-            <li className='nav-item'>
-                <div className='navicon'><NotificationsIcon/></div>
-                <span className='navtext'>Notifications</span>
+            <li className='nav-list'>
+                <div className='nav-item'>
+                    <div className='navicon'><NotificationsIcon/></div>
+                    <span className='navtext'>Notifications</span>
+                </div>
             </li>
-            <li className='nav-item'>
-                <div className='navicon'><CreateIcon/></div>
-                <span className='navtext'>Create</span>
+            <li className='nav-list'>
+                <div className='nav-item'>
+                    <div className='navicon'><CreateIcon/></div>
+                    <span className='navtext'>Create</span>
+                </div>
             </li>
-            <li className='nav-item'>
-                <div className='navicon'><TempI/></div>
-                <span className='navtext'>Profile</span>
+            <li className='nav-list'>
+                <div className='nav-item'>
+                    <div className='navicon'><TempI/></div>
+                    <span className='navtext'>Profile</span>
+                </div>
+            </li>
+            <div className='nav-space'></div>
+            <li className='nav-list'>
+                <div className='nav-more'>
+                    <div className='navicon'><MoreIcon/></div>
+                    <span className='navtext'>More</span>
+                </div>
             </li>
         </ul>
     </nav>);
