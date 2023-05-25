e58eaee76e2ae437812072864c74058592a2d5bb
Shahria Abeed <shahriaabeed@gmail.com>
2023-05-25 16:07:19 -0400

    deleted some startup files & started routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,34 @@
 import React from 'react';
-import './App.css';
 import NavBar from './navbar';
+import { Route, Routes } from 'react-router-dom';
 
+
+// TODO: instagrams root page is also the sign-in, need to check if user is logged in
+// TODO: move navbar to specific page's elements when they are completed
+// Only Explore and Profile lead to new pages
 function App() {
-  return <NavBar/>;
+  return ( <Routes>
+    
+    {/* Home Path */}
+    <Route path="/" element={
+      <NavBar/>
+    }/>
+
+    {/* Explore Path */}
+    <Route path="/explore/"/>
+
+    {/* 
+      Profile Route 
+      The path should be the username/tag of the user
+    */}
+    {/*<Route path="" element={
+      <NavBar/>
+    }/>*/}
+
+  </Routes>
+
+
+  );
 }
 
 export default App;
