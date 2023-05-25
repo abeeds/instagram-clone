657ce98727f40fa2e21b4f3e14c37558971f59b0
Shahria Abeed <shahriaabeed@gmail.com>
2023-05-25 16:56:25 -0400

    removed navbar import from app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import NavBar from './navbar';
 import { Route, Routes } from 'react-router-dom';
 
 import Home from './home page/home';
 import Explore from './explore page/explore';
 
 
-// TODO: instagrams root page is also the sign-in, need to check if user is logged in
+// TODO: instagram's root page is also the sign-in, need to check if user is logged in
 // TODO: move navbar to specific page's elements when they are completed
 // Only Explore and Profile lead to new pages
 function App() {
