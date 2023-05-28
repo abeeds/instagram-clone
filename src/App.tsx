e56eab1a89deb6d65595b2831c54dfd16753c662
Shahria Abeed <shahriaabeed@gmail.com>
2023-05-28 16:05:36 -0400

    Converted from react-app to vite

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,28 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 
-import Home from './home page/home';
-import Explore from './explore page/explore';
+import Home from './home page/home.tsx';
+import Explore from './explore page/explore.tsx';
 
 
 // TODO: instagram's root page is also the sign-in, need to check if user is logged in
 // TODO: move navbar to specific page's elements when they are completed
 // Only Explore and Profile lead to new pages
 function App() {
-  return ( <Routes>
+  return ( 
+    <Routes>
     
-    {/* Home Path */}
-    <Route path="/" element={ <Home/> }/>
+      {/* Home Path */}
+      <Route path="/" element={ <Home/> }/>
 
-    {/* Explore Path */}
-    <Route path="/explore/" element={ <Explore/> }/>
+      {/* Explore Path */}
+      <Route path="/explore/" element={ <Explore/> }/>
 
-    {/* 
-      Profile Route 
-      The path should be the username/tag of the user
-    */}
-    {/*<Route path="" element={ <NavBar/> }/>*/}
+      {/* 
+        Profile Route 
+        The path should be the username/tag of the user
+      */}
+      {/*<Route path="" element={ <NavBar/> }/>*/}
 
   </Routes>
 
