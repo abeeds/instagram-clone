c104624ff539ee362cff2a5c1d5b77a6aa682f29
Shahria Abeed <shahriaabeed@gmail.com>
2023-05-30 18:29:14 -0400

    implemented some post options (like comment save)

--- src/home page/home.tsx
+++ src/home page/home.tsx
@@ -2,6 +2,10 @@ import React from 'react';
 import NavBar from '../navbar.tsx';
 import "./home.css";
 import {ReactComponent as TempI} from '../images/temp.svg';
+import {ReactComponent as Like} from './post options/like.svg';
+import {ReactComponent as Save} from './post options/save.svg';
+import {ReactComponent as Comment} from './post options/comment.svg';
+
 
 /*
     This function will display an image with the user
@@ -11,7 +15,10 @@ import {ReactComponent as TempI} from '../images/temp.svg';
 type PostProps = {
     username: string,
     image: string,
-    location?: string
+    location?: string,
+    liked_by?: string,
+    like_count: number,
+    comment_count: number
 };
 
 
@@ -37,12 +44,27 @@ const PostImage = (props: PostProps) => {
 
             {/* Like, Comment. Save on far right */}
             <div className='post-options'>
+                <div className='post-like'>
+                    <Like/>
+                </div>
+                <div className='post-comment'>
+                    <Comment/>
+                </div>
 
+                <div className='post-save'>
+                    <Save/>
+                </div>
             </div>
 
             {/* Liked by, then caption, view comments with num of comments, then  */}
             <div className='post-info'>
-
+                
+                {/*Only add view option if there are more than 0 comments*/}
+                {props.comment_count > 0 ? <>
+                        <span className='post-comment-option'> View all {props.comment_count} comments</span> 
+                        <br />
+                    </>: null}
+                <span className='post-comment-option'>Add a comment...</span>
             </div>
         </div>
     )
@@ -60,6 +82,8 @@ function Home() {
                     <PostImage 
                         username={'Joey'} 
                         image={"https://images.unsplash.com/photo-1558788353-f76d92427f16?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8Z29sZGVuJTIwZG9nfGVufDB8fDB8fHww&w=1000&q=80"}
+                        like_count={23}
+                        comment_count={13}
                     />
                 </div>
             </div>
