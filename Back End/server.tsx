db1868fa7efdd6cb48e75cac4ee74073a5e1756c
Shahria Abeed <shahriaabeed@gmail.com>
2023-08-02 13:41:41 -0400

    connected to db

--- Back End/server.tsx
+++ Back End/server.tsx
@@ -1,4 +1,18 @@
 const express = require('express');
-const app = express()
+const mongoose=require('mongoose');
+const app = express();
+const port = 3000;
+const mongoDB = "mongodb://localhost:27017/myDB";
 
-app.listen(3000)
+// I am using a local database 
+// useNewUrlParser and useUnifiedTopology are true to avoid deprecation warnings
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true })
+    // On Resolved
+    .then(() => {
+        app.listen(port);
+    })
+    
+    // Error Handling
+    .catch((err: any) => {
+        console.error("Error connecting to database:", err);
+    });
