6cab34e17d314f08a544dc4d04d8b096b84a8545
Shahria Abeed <105658920+abeeds@users.noreply.github.com>
2023-09-07 12:45:09 -0400

    added form fields for login + CSS

--- Front End/src/login page/login.tsx
+++ Front End/src/login page/login.tsx
@@ -4,8 +4,22 @@ function LogInBox() {
     return ( 
         <div className="LogInBox">
             <a id='LogInText'>Log In</a>
-            <a id='LogInUsername'>Username</a>
-            <a id='LogInPassword'>Password</a>
+
+            {/* Form */}
+            <form className='LogInFields' method="post">
+                <label>
+                    Username<br/>
+                    <input id="username" type="text" name="username" maxLength={64} required/>
+                </label>
+
+                <label>
+                    Password<br/>
+                    <input id="password" type="password" name="password" maxLength={32} required/>
+                </label>
+
+                <input type='submit' value='Submit'/>
+            </form>
+
         </div>
     )
 }
